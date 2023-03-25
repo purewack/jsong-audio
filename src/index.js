469ab29c54347961e8e021f6c6012dd40ae1db15
purewack <bobimaster15@gmail.com>
2023-03-25 21:28:13 +0000

    [dev] stop button, debug metronome

--- src/index.js
+++ src/index.js
@@ -3,42 +3,63 @@ import * as Tone from "tone";
 
 import song from './manifest.json'
 
-const osc = new Tone.Synth().toDestination();
 const audioButton = document.getElementById("audio");
 audioButton.addEventListener("click", () => {
   Tone.start();
-  osc.triggerAttackRelease("c4", "4n");
 });
-
 
 class JSONPlayer {
 
-  parse(data){
+  parse(data, verbose){
     this.manifest = structuredClone(data)
     const players = []
     for(const track of this.manifest.tracks){
-      console.log(track)
+      if(verbose) console.log(track)
       const player = new Tone.Player(track.source).toDestination()
       player.volume.value = track.volumeDB
       players.push(player)
     }
     this.players = players
+
+    this.metronome = new Tone.Synth().toDestination()
+    this.metronome.envelope.attack = 0;
+    this.metronome.envelope.release = 0.05;
+    this.metronome.volume.value = -12;
+
+    Tone.Transport.cancel()
+    Tone.Transport.scheduleRepeat((t)=>{
+      this.metronome.triggerAttackRelease('b6','32n');
+    },'4n');
+    Tone.Transport.bpm.value = this.manifest.bpm
+    Tone.Transport.timeSignature = this.manifest.meter
   }
 
   start(){
+    Tone.Transport.start()
     this.players.forEach((p)=>{
-      p.start()
+      p.start("@1m")
+    })
+  }
+
+  stop(){
+    Tone.Transport.stop()
+    this.players.forEach((p)=>{
+      p.stop()
     })
   }
 
   constructor(data, verbose){
-    this.parse(data)
-    if(verbose) console.log(this)
+    this.parse(data, verbose)
   }
 }
 
 const player = new JSONPlayer(song, true)
+
 const playButton = document.getElementById("play");
 playButton.addEventListener("click", () => {
   player.start()
+});
+const stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", () => {
+  player.stop()
 });
