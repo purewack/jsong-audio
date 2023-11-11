eaa52164056adceba759a2fa4d921a980a4e40af
Damian Nowacki <bobimaster15@gmail.com>
2023-11-11 14:35:25 +0000

    [dev] temp grid align quantime while loop

--- src/quantime.test.js
+++ src/quantime.test.js
@@ -32,11 +32,35 @@ test('quantize time to nearest 2bar',()=>{
     expect(quanTime('2:1:2',8,[4,4])).toBe('4:0:0')
     expect(quanTime('3:1:2',8,[4,4])).toBe('5:0:0')
     expect(quanTime('3:1:2',8,[4,4])).toBe('5:0:0')
-
-    expect(quanTime('0:2:2',8,[4,4],1)).toBe('3:0:0')
-    expect(quanTime('1:1:2',8,[4,4],0)).toBe('2:0:0')
 })
 
+
+test('quantize time to nearest 2bar late gridAlign',()=>{
+    expect(quanTime('2:1:2',8,[4,4],'2:0:0')).toBe('4:0:0')
+    expect(quanTime('3:1:2',8,[4,4],'2:0:0')).toBe('4:0:0')
+    expect(quanTime('3:3:3',8,[4,4],'2:0:0')).toBe('4:0:0')
+    // expect(quanTime('3:3:3',8,[4,4],-2)).toBe('5:0:0')
+})
+
+test('quantize time to nearest 4bar late gridAlign',()=>{
+    expect(quanTime('2:1:2',16,[4,4],'0:0:0')).toBe('4:0:0')
+    expect(quanTime('3:1:2',16,[4,4],'0:0:0')).toBe('4:0:0')
+
+    expect(quanTime('4:1:2',16,[4,4],'4:0:0')).toBe('8:0:0')
+    expect(quanTime('5:2:0',16,[4,4],'4:0:0')).toBe('8:0:0')
+    expect(quanTime('6:3:0',16,[4,4],'4:0:0')).toBe('8:0:0')
+    expect(quanTime('7:2:0',16,[4,4],'4:0:0')).toBe('8:0:0')
+
+    expect(quanTime('2:3:3',16,[4,4],'2:0:0')).toBe('6:0:0')
+    expect(quanTime('4:3:3',16,[4,4],'2:0:0')).toBe('6:0:0')
+    expect(quanTime('5:3:3',16,[4,4],'2:0:0')).toBe('6:0:0')
+})
+
+test('quantize time to nearest 4bar late gridAlign multiple loops already',()=>{
+    expect(quanTime('8:3:3', 16,[4,4],'2:0:0')).toBe('10:0:0')
+    expect(quanTime('10:3:3',16,[4,4],'2:0:0')).toBe('14:0:0')
+    expect(quanTime('13:3:3',16,[4,4],'2:0:0')).toBe('14:0:0')
+})
 
 })
 
