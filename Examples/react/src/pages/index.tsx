f782a4b696841e1164761397309d86426992f740
Damian Nowacki <bobimaster15@gmail.com>
2023-11-15 23:51:00 +0000

    [examples] add react (next) setup files

---
+++ Examples/react/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/index.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>
+          What is JSONg audio?
+        </h1>
+      </main>
+    </>
+  )
+}
