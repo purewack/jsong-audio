02e0943e00830efc4903bb86f94346381d0489d7
Damian Nowacki <bobimaster15@gmail.com>
2023-12-25 22:12:30 +0000

    [dev] simple toneaudiobuffer loading test

--- tests/JSONgBuffers.test.ts
+++ tests/JSONgBuffers.test.ts
@@ -28,33 +28,35 @@ test('base URL resolution', ()=>{
 
 describe('ToneAudioBuffer mock', () => {
 
-    const mockManifest: Partial<JSONgManifestFile> = {
-        type: 'jsong',
-        baseURL: undefined,
-        sources: {
-            'one':'path/to/audio1.mp3', 
-            'two':'path/to/audio2.mp3'
-        },
-    }
-
     test('should load audio files successfully', async () => {
+        const mockManifest: Partial<JSONgManifestFile> = {
+            type: 'jsong',
+            baseURL: undefined,
+            sources: {
+                'one':'path/to/audio1.mp3', 
+                'two':'path/to/audio2.mp3'
+            },
+        }
+
         const mockToneAudioBuffer = {
-            load: jest.fn().mockResolvedValue({/* mock return value */}),
+            load: jest.fn().mockResolvedValue(new Promise(resolve=>{
+                resolve(new ToneAudioBuffer());
+            })),
         };
     
         (ToneAudioBuffer as unknown as jest.Mock).mockImplementation((filePath: string) => {
             return mockToneAudioBuffer;
         });
 
-
         const filePathsKeys = Object.keys(mockManifest.sources as object);
-        const loadedBuffers = await loadBuffers(mockManifest as JSONgManifestFile);
+        const loadedBuffers = await loadBuffers(mockManifest as JSONgManifestFile, 'all');
     
-        expect(loadedBuffers.length).toBe(filePathsKeys.length);
-    
+        expect(Object.keys(loadedBuffers).length).toBe(filePathsKeys.length);
+        
         // You can add more specific assertions based on your needs
-        expect(ToneAudioBuffer.load).toHaveBeenCalledTimes(filePathsKeys.length);
-        expect(ToneAudioBuffer.load).toHaveBeenCalledWith(filePathsKeys[0]);
-        expect(ToneAudioBuffer.load).toHaveBeenCalledWith(filePathsKeys[1]);
+        expect(mockToneAudioBuffer.load).toHaveBeenCalledTimes(filePathsKeys.length);
+        expect(mockToneAudioBuffer.load).toHaveBeenCalledWith((mockManifest.sources as JSONgDataSources).one);
+        expect(mockToneAudioBuffer.load).toHaveBeenCalledWith((mockManifest.sources as JSONgDataSources).two);
     });
+    
   });
