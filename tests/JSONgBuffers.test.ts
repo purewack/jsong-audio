541254e9e75752e32c670f3f33c458af73c2b48d
Damian Nowacki <bobimaster15@gmail.com>
2023-12-25 22:41:14 +0000

    [dev] loadBuffers reject test

--- tests/JSONgBuffers.test.ts
+++ tests/JSONgBuffers.test.ts
@@ -44,12 +44,10 @@ describe('ToneAudioBuffer mock', () => {
             })),
         };
     
-        (ToneAudioBuffer as unknown as jest.Mock).mockImplementation((filePath: string) => {
-            return mockToneAudioBuffer;
-        });
+        (ToneAudioBuffer as unknown as jest.Mock).mockImplementation(() => mockToneAudioBuffer );
 
         const filePathsKeys = Object.keys(mockManifest.sources as object);
-        const loadedBuffers = await loadBuffers(mockManifest as JSONgManifestFile, 'all');
+        const loadedBuffers = await loadBuffers(mockManifest as JSONgManifestFile);
     
         expect(Object.keys(loadedBuffers).length).toBe(filePathsKeys.length);
         
@@ -57,6 +55,32 @@ describe('ToneAudioBuffer mock', () => {
         expect(mockToneAudioBuffer.load).toHaveBeenCalledTimes(filePathsKeys.length);
         expect(mockToneAudioBuffer.load).toHaveBeenCalledWith((mockManifest.sources as JSONgDataSources).one);
         expect(mockToneAudioBuffer.load).toHaveBeenCalledWith((mockManifest.sources as JSONgDataSources).two);
+    });
+
+    test('should fail to load audio files as one is missing', async () => {
+        const mockManifest: Partial<JSONgManifestFile> = {
+            type: 'jsong',
+            baseURL: undefined,
+            sources: {
+                'one':'path/to/audio1.mp3', 
+                'two':'fail/to/audio2.mp3'
+            },
+        }
+
+        const mockToneAudioBuffer = {
+            load: jest.fn().mockImplementation((filePath: string)=>{
+                return filePath.includes('fail') 
+                ? 
+                    Promise.reject(new Error('cannot load'))
+                :
+                    Promise.resolve(new ToneAudioBuffer());
+            })
+        };
+    
+        (ToneAudioBuffer as unknown as jest.Mock).mockImplementation(() => mockToneAudioBuffer);
+
+        const buffers = await expect(loadBuffers(mockManifest as JSONgManifestFile)).rejects.toThrow('cannot load');
+        // expect(Object.keys(buffers).length)
     });
     
   });
