c60af7a3384c707a3428ac5dbefeb248ef294071
purewack <bobimaster15@gmail.com>
2026-01-18 15:52:09 +0000

    [dev] fix for tests not running under module configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
